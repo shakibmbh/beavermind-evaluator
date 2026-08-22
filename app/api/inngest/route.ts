e8ee06bd2718d3a8cee33349f978138d2e8099bb
shakibmbh <shakimbhb2@gmail.com>
2026-08-22 21:26:11 +0600

    Delete project files

--- app/api/inngest/route.ts
+++
@@ -1,7 +0,0 @@
-import { serve } from "inngest/next";
-import { inngest, evaluateTranscript } from "@/lib/inngest";
-
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [evaluateTranscript],
-});
